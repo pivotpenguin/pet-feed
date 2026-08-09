bfc588e2f623afab109f599450017971249c9d04
pivotpenguin <pivotpenguin@gmail.com>
2026-08-09 18:15:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function saveFedPets() {
   localStorage.setItem(storageKey, JSON.stringify({ day: dayKey, pets: fedPets }));
 }
 
-fusnction render() {
+function render() {
   const list = document.getElementById("petList");
   list.innerHTML = pets.map((pet, index) => {
     const isFed = fedPets.includes(pet.id);
